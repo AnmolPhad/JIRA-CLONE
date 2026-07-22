14fc09dfa9d863186f1046a4e55ac07292a52246
AnmolPhad <120477856+AnmolPhad@users.noreply.github.com>
2026-07-23 00:34:25 +0530

    routing problem

--- frontend/src/Components/Main/Hero.jsx
+++ frontend/src/Components/Main/Hero.jsx
@@ -1,23 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
-import Board from "../Board/Board";
 
 function Hero() {
   return (
     <div className="h-screen bg-[#FAFBFC] overflow-hidden">
-      {/* Left Blue Navbar */}
+      {/* Blue Navbar */}
       <Navbar />
 
       {/* Sidebar */}
       <Sidebar />
 
       {/* Main Content */}
-<main className="ml-[304px] px-8 py-6">
-    <div className="max-w-[1200px]">
-        <Board />
-    </div>
-</main>
+      <main className="ml-[304px] px-8 py-6">
+        <div className="max-w-[1200px]">
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 }
