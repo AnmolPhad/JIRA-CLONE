a9e1c506159e5fd01ca24558f34e45a9cbcde4a5
AnmolPhad <120477856+AnmolPhad@users.noreply.github.com>
2026-07-24 07:22:39 +0530

    backend setup spring boot project added

---
+++ backend/jira-backend/src/test/java/com/backend/jira/JiraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.jira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
